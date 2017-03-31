9903a6504d66a71f97e0f4cfda5a33f3511a9df4
Zsolt Fabok <me@zsoltfabok.com>
2017-03-31 16:40:41 +0200

    add a basic REST query to see how RestTemplate works

--- src/main/java/com/zsoltfabok/LoginController.java
+++ src/main/java/com/zsoltfabok/LoginController.java
@@ -1,12 +1,25 @@
 package com.zsoltfabok;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
+@RequestMapping("/login")
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
+    @RequestMapping(method=RequestMethod.GET)
     public String login() {
+        return "login";
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public String loginSuccess(Model model) {
+        RestTemplate template = new RestTemplate();
+        JsonNode json = template.getForObject("http://gturnquist-quoters.cfapps.io/api/random", JsonNode.class);
+        model.addAttribute("quote", json.get("value").get("quote"));
         return "login";
     }
 }
