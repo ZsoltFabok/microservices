a1736b6d594bb6ddf11669321d8ee110d33d83e1
Zsolt Fabok <me@zsoltfabok.com>
2017-05-06 14:15:08 +0200

    [webservice] fix login redirect

--- webservice/src/test/java/com/zsoltfabok/webservice/web/HomeControllerTest.java
+++ webservice/src/test/java/com/zsoltfabok/webservice/web/HomeControllerTest.java
@@ -7,8 +7,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
@@ -19,10 +23,21 @@ public class HomeControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void showsTheMainPage() throws Exception {
+    public void showsTheMainPageWhenTheUserIsNotLoggedIn() throws Exception {
         mvc.perform(get("/"))
-            .andExpect(status().isOk())
-            .andExpect(xpath("/html/head/title").string(containsString("Microservices")));
+            .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    public void showsTheMainPageWhenTheUserIsLoggedIn() throws Exception {
+        Map<String, Object> session = new HashMap<>();
+        session.put("id", "zsolt");
+
+        mvc.perform(get("/")
+                .sessionAttrs(session))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/head/title").string(containsString("Microservices")));
     }
 }
 
